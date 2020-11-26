4f9f598c055cf8ab6f63b44f0c9105706f5a809d
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2020-11-26 10:09:32 +0000

    Add project files.

---
+++ Week8/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week8
+{
+    class Player
+    {
+        public int PlayerID { get; set; }
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+
+
+    }
+}
